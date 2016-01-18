b11e6775c7957ca8970db544f6d2a36402b5eec1
Sebastien Martin <semartin@mit.edu>
2016-01-18 11:57:12 -0500

    added parallel shortest paths

--- src/RoutingNetworks.jl
+++ src/RoutingNetworks.jl
@@ -14,6 +14,7 @@ export squareNetwork, urbanNetwork
 
 #routing
 export RoutingTimes, routingDistances, getRoutingTimes, getPath, shortestPaths!
+export parallelShortestPaths!
 
 #visualization
 export visualize
--- src/routing/shortestpaths.jl
+++ src/routing/shortestpaths.jl
@@ -23,3 +23,37 @@ function shortestPaths!(r::RoutingTimes)
     r.previousNode=parents
     return r
 end
+
+"""
+    Compute shortest path using the given timings
+    --parallel version, uses all available processors by default
+"""
+function parallelShortestPaths!(r::RoutingTimes)
+
+    if length(workers()) <= 1
+        println("Only one thread: use shortestPaths! instead")
+    end
+
+    @everywhere using LightGraphs
+
+
+    @everywhere g = r.network.graph
+    if !is_strongly_connected(g)
+        error("The network needs to be strongly connected")
+    end
+
+    #creating the workers
+
+    fullTimes = SharedArray(Float64, (nv(g), nv(g)))
+    parents  = SharedArray(Int,(nv(g),nv(g)))
+
+     @sync @parallel for orig in 1:nv(g)
+        d = dijkstra_shortest_paths(g, orig, r.times)
+        fullTimes[orig,:] = d.dists
+        parents[orig,:] = d.parents
+    end
+    rmpro
+    r.fullTimes=fullTimes
+    r.previousNode=parents
+    return r
+end
